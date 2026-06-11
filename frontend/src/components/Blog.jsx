c672959ab7481729ee0c516aeb244c83566adfb5
Santiago Ariel Alvarez <santiiariel2004@gmail.com>
2026-06-11 12:18:52 -0300

    Excersice 5.7

--- frontend/src/components/Blog.jsx
+++ frontend/src/components/Blog.jsx
@@ -1,7 +1,25 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import "../styles/blog.css";
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [visibile, setVisibile] = useState(false);
+
+  const visibileDetail = { display: visibile ? "" : "none" };
+  return (
+    <div className="blog">
+      {blog.title}
+      <button onClick={() => setVisibile(!visibile)}>
+        {visibile ? "hide" : "view"}
+      </button>
+      <div style={visibileDetail}>
+        <p>url: {blog.url}</p>
+        <p>
+          likes: {blog.likes} <button>like</button>
+        </p>
+        <p>author: {blog.author}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
